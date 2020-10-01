54bc61104a8d03a5c5c7ab46e3ae55575532f077
Denovsky <62519665+Denovsky@users.noreply.github.com>
2020-10-01 10:14:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express() 
 
-const PORT = 3000
+const PORT = process.env.PORT || 80
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
